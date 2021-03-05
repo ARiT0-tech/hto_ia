079cfaf7565c9202bd9956374ce574a90d850991
savage-lof <lomova20042004@ya.ru>
2021-03-05 17:29:37 +0300

    cum2

--- srv.py
+++ srv.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -8,6 +9,14 @@ app = Flask(__name__)
 def index():
     title = 'mars'
     return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    title = 'mars'
+    prof = prof.lower().split(' ')
+    print(prof)
+    return render_template('base.html', title=title, prof=prof)
 
 
 if __name__ == '__main__':
